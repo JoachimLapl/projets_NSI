cdd8c504d37d83f5d1f3fccdf5bc7e29091a3491
JoachimLapl <joachim.laplanche@gmail.com>
2021-12-24 12:58:37 +0100

    change map

--- jeu/map_utils2.js
+++ jeu/map_utils2.js
@@ -280,7 +280,8 @@ class Wheel {
             size = (size - PR) * sin;
             console.log('size', size)
             vector = Vector.setToNorm(v_QR, size);
-            this.map.svg.insertAdjacentHTML('beforeend', `<line x1=${R[0] - 50} y1=${R[1] - 50} x2=${R[0] - 50 + vector[0]} y2=${R[1] - 50 + vector[1]} style="stroke:#00f;stroke-width:.4" />`)
+            Vector.draw(vector, Point.translate(R,[-50,-50]))
+            // this.map.svg.insertAdjacentHTML('beforeend', `<line x1=${R[0] - 50} y1=${R[1] - 50} x2=${R[0] - 50 + vector[0]} y2=${R[1] - 50 + vector[1]} style="stroke:#00f;stroke-width:.4" />`)
             // changer la varibale`vector` en fonction du vecteur du`mur` et de la distance où se trouve le point issu de la translation de`point` par le vecteur`vector` partant du point`nouveau_point`
             point = R;
             // break; // to be removed later
