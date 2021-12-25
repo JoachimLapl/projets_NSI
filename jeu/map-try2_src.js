2e115488066b743b7bf95b9c7ec7e13fbcab8f9c
JoachimLapl <joachim.laplanche@gmail.com>
2021-12-25 14:27:01 +0100

    show droite

--- jeu/map-try2_src.js
+++ jeu/map-try2_src.js
@@ -38,6 +38,7 @@ drt4.draw(map.svg, [-50, -50, 50, 50])
 const drt5 = MAP.WALL[.25][2][0].line
 const drt6 = MAP.WALL[.25][7][0].line
 
+console.log(drt5[2],drt6[2]);
 drt5.translate(0,0)
 drt6.translate(0,1)
 
